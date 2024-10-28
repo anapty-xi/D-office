3e773b1d6e5eeb008fb3d009d1ee8a888c38bc82
anapty-xi <mironovn2006@gmail.com>
2024-10-28 13:22:34 +0300

    Add files via upload

--- booking/admin.py
+++ booking/admin.py
@@ -2,32 +2,66 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
 from django.contrib.auth.models import User, Group
 from django.contrib import admin
+from numba.scripts.generate_lower_listing import description
 from unfold.admin import ModelAdmin
 from .models import maps, workplace, booking
+from unfold.contrib.filters.admin import RangeDateFilter, FieldTextFilter
+from unfold.decorators import action
+from django.shortcuts import render
+from unfold.contrib.filters.admin import (
+    ChoicesDropdownFilter,
+    MultipleChoicesDropdownFilter,
+    RelatedDropdownFilter,
+    MultipleRelatedDropdownFilter,
+    DropdownFilter,
+    MultipleDropdownFilter
+)
+
 
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
-
-
 @admin.register(User)
 class UserAdmin(BaseUserAdmin, ModelAdmin):
-    pass
-
+    list_filter_submit = True
+    list_filter=[
+        ('username', FieldTextFilter),
+        ('email', FieldTextFilter),
+        ('first_name', FieldTextFilter),
+        ('last_name', FieldTextFilter),
+    ]
 
 @admin.register(Group)
 class GroupAdmin(BaseGroupAdmin, ModelAdmin):
     pass
 
 
+
+
+
 @admin.register(workplace)
 class Workplace(ModelAdmin):
-    empty_value_display = 'No information'
+    ordering = ['workplace_code']
+    list_filter_submit = True
+    list_display = ['workplace_code', 'equipment', 'map_id']
+    list_filter = [
+        ('workplace_code', FieldTextFilter),
+        ('equipment', FieldTextFilter),
+        'map_id'
+    ]
+
 
 
 @admin.register(booking)
 class Booking(ModelAdmin):
-   pass
+    ordering = ['date']
+    list_display = ['owner_id', 'workplace_id', 'date']
+    list_filter_submit = True
+    list_filter = [
+       ('date', RangeDateFilter),
+       ('owner_id', FieldTextFilter)
+    ]
+
 
 
 @admin.register(maps)
--- booking_project/settings.py
+++ booking_project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.urls import reverse_lazy
+import unfold
+from django.conf.urls.static import static
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,6 +50,20 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'booking.apps.BookingConfig'
 ]
+
+UNFOLD = {
+    'SITE_TITLE': 'Администрирование D-office',
+    'SITE_HEADER': 'Администрирование',
+
+
+}
+
+
+
+
+
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
